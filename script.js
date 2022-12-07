85e4d95528f15de8458dd6514abd0aad36fd0f81
Wellington Galvao <wellingtongalvao96@gmail.com>
2022-12-07 13:25:11 -0300

    Adicionando evento click aos botoes

---
+++ script.js
@@ -0,0 +1,12 @@
+let shuffle = document.getElementById('shuffle')
+let playSkipBack = document.getElementById('play-skip-back')
+let play = document.getElementById('play')
+let playSkipForward = document.getElementById('play-skip-forward')
+let repeat = document.getElementById('repeat')
+
+shuffle.addEventListener('click', () => console.log('shuffle'))
+playSkipBack.addEventListener('click', () => console.log('playSkipBack'))
+play.addEventListener('click', () => console.log('play'))
+playSkipForward.addEventListener('click', () => console.log('playSkipForward'))
+repeat.addEventListener('click', () => console.log('repeat'))
+
